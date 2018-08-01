8d573a201a6f363986b534e6018852c063d5e471
hlippke <hl@my-boathouse.com>
2018-08-01 11:02:18 +0200

    Update start.sh
    - wait for jack master
    - exit when stereotool exits

--- start.sh
+++ start.sh
@@ -11,32 +11,17 @@ if [ $status -ne 0 ]; then
   exit $status
 fi
 
-sleep 1
+# wait for JACK to get connect to master
+while sleep 1; do
+	SYSTEM_PORTS="$(jack_lsp 2> /dev/null | grep system)"
 
-# Start stereotool
-./stereo_tool_gui_jack_64 &
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start StereoTool: $status"
-  exit $status
-fi
-
-# Check once a minute to see if either of the processes exited.
-# The container exits with an error
-# if it detects that either of the processes has exited.
-# Otherwise it loops forever, waking up every 60 seconds
-
-while sleep 60; do
-  ps aux |grep jackd |grep -q -v grep
-  PROCESS_1_STATUS=$?
-  ps aux |grep stereo_tool_gui_jack_64 |grep -q -v grep
-  PROCESS_2_STATUS=$?
-  # If the greps above find anything, they exit with 0 status
-  # If they are not both 0, then something is wrong
-  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 ]; then
-    echo "One of the processes has already exited."
-    exit 1
-  fi
+	if [ -n "$SYSTEM_PORTS" ]; then
+		 echo $SYSTEM_PORTS
+		break
+	fi
 done
 
+# Start stereotool (blocks until terminated)
+./stereo_tool_gui_jack_64 
 
+### TODO: cleanup JACK on exit
